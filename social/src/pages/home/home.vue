2532a009edfb0160348bf42883c7a7d351664390
UlissesMesquita <souza.mesquita@gmail.com>
2020-09-04 11:59:11 -0300

    Explicação sobre pastas do Vue

---
+++ social/src/pages/home/home.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="">
+        <h2> Social </h2>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'home',
+  data () {
+    return {
+      
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
+
