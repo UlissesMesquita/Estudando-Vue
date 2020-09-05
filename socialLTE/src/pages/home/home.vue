6587abafb3369f32458e2053cf03915591027ed5
Ulisses Mesquita <souza.mesquita@gmail.com>
2020-09-05 12:38:15 -0300

    Continuando o processo com o social

---
+++ socialLTE/src/pages/home/home.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="">
+        <h2> Social </h2>
+
+
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'home',
+  data () {
+    return {
+      
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
+
