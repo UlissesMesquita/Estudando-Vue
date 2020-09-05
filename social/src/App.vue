5e683b23ab34e9c31dc384c70228472003874318
Ulisses Mesquita <souza.mesquita@gmail.com>
2020-09-05 14:16:00 -0300

    Rodapé Fixado

--- social/src/App.vue
+++ social/src/App.vue
@@ -1,57 +1,25 @@
 <template>
-  <span>
+<span>
 
-<header>
+  <header>
       <nav-bar cor="green darken-1" logo="Social Media" url="#"/>
-</header>
+  </header>
 
-<main>
-  <div class="container"></div>
-    <router-view/>
-</main>
+  <main>
+    <div class="container"></div>
+      <router-view/>
+  </main>
 
+    <footer-vue cor="green darken-1" logo="Social" descricao="Informações Sobre o Social" ano="2020"/>
 
-     <footer class="page-footer green darken-1">
-          <div class="container">
-            <div class="row">
-              <div class="col l6 s12">
-                <h5 class="white-text">Footer Content</h5>
-                <p class="grey-text text-lighten-4">You can use rows and columns here to organize your footer content.</p>
-              </div>
-              <div class="col l4 offset-l2 s12">
-                <h5 class="white-text">Links</h5>
-                <ul>
-                  <li><a class="grey-text text-lighten-3" href="#!">Link 1</a></li>
-                  <li><a class="grey-text text-lighten-3" href="#!">Link 2</a></li>
-                  <li><a class="grey-text text-lighten-3" href="#!">Link 3</a></li>
-                  <li><a class="grey-text text-lighten-3" href="#!">Link 4</a></li>
-                </ul>
-              </div>
-            </div>
-          </div>
-          <div class="footer-copyright">
-            <div class="container">
-            © 2014 Copyright Text
-            <a class="grey-text text-lighten-4 right" href="#!">More Links</a>
-            </div>
-          </div>
-        </footer>
-
-
-
-
-
-
-
-
-  </span>
+</span>
 </template>
 
 <script>
 //Importação de NavBars
   //Components - Layouts
     import NavBar from '@/components/layouts/NavBar'
-    import Footer from '@/components/layouts/Footer'
+    import FooterVue from '@/components/layouts/FooterVue'
 
 
 //Importação de componentes
@@ -59,12 +27,21 @@ export default {
   name: 'App',
   components: {
     NavBar,
-    Footer
+    FooterVue
   }
 }
 </script>
 
 
-<style >
+<style scoped>
 
+  main {
+    display: flex;
+    min-height: 100vh;
+    flex-direction: column;
+  }
+
+  main {
+    flex: 1 0 auto;
+  }  
 </style>
---
+++ social/src/components/layouts/FooterVue.vue
@@ -0,0 +1,51 @@
+<template>
+
+     <footer :class="cor || 'blue'">
+          <div class="container">
+            <div class="row">
+              <div class="col l6 s12">
+                <h5 class="white-text">{{footerName || 'Footer Name'}}</h5>
+                <p class="grey-text text-lighten-4">{{subFooter || 'Descrição Footer'}}</p>
+              </div>
+              <div class="col l4 offset-l2 s12">
+                <h5 class="white-text">Links</h5>
+                <ul>
+                  <li><a class="grey-text text-lighten-3" href="#!">Link 1</a></li>
+                  <li><a class="grey-text text-lighten-3" href="#!">Link 2</a></li>
+                  <li><a class="grey-text text-lighten-3" href="#!">Link 3</a></li>
+                  <li><a class="grey-text text-lighten-3" href="#!">Link 4</a></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div class="footer-copyright">
+            <div class="container">
+            © {{ano}} {{logo}}
+            <a class="grey-text text-lighten-4 right" href="#!">More Links</a>
+            </div>
+          </div>
+        </footer>
+
+
+
+</template>
+
+
+<script>
+export default {
+  name: 'FooterVue',
+  props:['cor', 'logo', 'descricao','ano'],
+  data () {
+    return {
+      
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+
+
+</style>
+
