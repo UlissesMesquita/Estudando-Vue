ce59cf792683a2f9a9728c2aa4c2e9287f3bc2bc
Ulisses Mesquita <souza.mesquita@gmail.com>
2020-09-05 13:11:01 -0300

    Alterando os :vbind: para alterar o nome dinamicamente

--- social/src/App.vue
+++ social/src/App.vue
@@ -1,14 +1,26 @@
 <template>
   <div>
+
+      <nav-bar cor="green darken-1" logo="Social Media" url="Hastag" menu1="Arquivos" menu2="Pastas" menu3="Feed"/>
+
     <router-view/>
   </div>
 </template>
 
 <script>
+//Importação de NavBars
+  import NavBar from '@/components/layouts/NavBar'
+
+
+//Importação de componentes
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    NavBar
+  }
 }
 </script>
+
 
 <style>
 
---
+++ social/src/components/layouts/NavBar.vue
@@ -0,0 +1,32 @@
+<template>
+
+  <nav :class="cor || 'blue' ">
+    <div class="nav-wrapper container">
+      <a :href="url || '#' " class="brand-logo">{{logo || 'Logo'}}</a>
+      <ul id="nav-mobile" class="right hide-on-med-and-down">
+        <li><a :href="menu1-url || '#' ">{{menu1 || 'Menu 1'}}</a></li>
+        <li><a :href="menu2-url || '#' ">{{menu2 || 'Menu 2'}}</a></li>
+        <li><a :href="menu3-url || '#' ">{{menu3 || 'Menu 3'}}</a></li>
+      </ul>
+    </div>
+  </nav>
+
+</template>
+
+
+<script>
+export default {
+  name: 'NavBar',
+  props:['logo', 'url', 'cor', 'menu1-url', 'menu2-url', 'menu3-url', 'menu1', 'menu2', 'menu3'],
+  data () {
+    return {
+      
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
+
