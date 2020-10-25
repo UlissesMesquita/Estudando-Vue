2e239f1044e21dccf28edf5f07a5d29b3fe32fec
Ulisses Mesquita <souza.mesquita@gmail.com>
2020-10-25 15:37:14 -0300

    Criar rotas de cadastro - 14:00

--- Api-Laravel/app/Models/User.php
+++ Api-Laravel/app/Models/User.php
@@ -8,11 +8,11 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
 use laravel\Passport\HasApiTokens;
-use HasApiTokens, Notifiable;
+
 
 class User extends Authenticatable
 {
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable, HasApiTokens;
 
     /**
      * The attributes that are mass assignable.
